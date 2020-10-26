3583dddbfc82dc3c694990bea8eed2c90418b2de
Kevin-Lai <kevinlai249@gmail.com>
2020-10-26 11:15:30 -0700

    Fixed user auth

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
-import tweets from './tweets_reducer';
 
 const RootReducer = combineReducers({
   errors,
