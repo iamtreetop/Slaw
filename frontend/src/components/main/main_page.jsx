48a357772a4e24594e7df09a7e1cd74413e85c7c
iamtreetop <trivta@gmail.com>
2020-10-26 20:28:08 -0700

    Add css to splash page

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./main_page.css"
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Welcome to Slaw</h1>
+      <div >
+        <div className="main-container" >
+          <h1  className="main-header">Welcome to Slaw</h1>
+        </div>  
         <footer>
           Copyright &copy; 2020 SlawApp
         </footer>
