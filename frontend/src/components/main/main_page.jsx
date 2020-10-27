c76938e5feb7d58d58e371a03eeecb8edbc8e13e
iamtreetop <trivta@gmail.com>
2020-10-26 21:55:31 -0700

    Add Splash

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -7,7 +7,11 @@ class MainPage extends React.Component {
     return (
       <div >
         <div className="main-container" >
-          <h1  className="main-header">Welcome to Slaw</h1>
+          <div className="main-message-wrapper">
+            <h1  className="main-header">Welcome to Slaw</h1>
+            <h2  className="sub-header">Your Place to Slaw </h2>
+            <h2  className="sub-header">Whether you’re strong like JD, buff like Jacky, or just looking for a workout partner, Slaw is  your place to connect.</h2>
+          </div>
         </div>  
         <footer>
           Copyright &copy; 2020 SlawApp
