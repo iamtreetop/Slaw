444c005f4843d045d682143b6e15aa004cecb808
Kevin-Lai <kevinlai249@gmail.com>
2020-10-27 12:29:37 -0700

    Add user's list of joined channels

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,11 +3,13 @@ import session from './session_reducer';
 import errors from './errors_reducer';
 import channels from "./channels_reducer";
 import uiReducer from "./ui_reducer"
+import users from "./users_reducer";
 
 const RootReducer = combineReducers({
   errors,
   session,
   channels,
+  users,
   ui: uiReducer
 });
 
