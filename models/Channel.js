e29e90fd6fac88fab38dc2858f54b870b176b579
jackyli97 <xwlh808@gmail.com>
2020-10-28 12:18:52 -0700

    Fix channels backend for patching

--- models/Channel.js
+++ models/Channel.js
@@ -30,10 +30,6 @@ const ChannelSchema = new Schema({
     },
     members: {
         type: [{ type: Schema.Types.ObjectId, ref:'User' }],
-    },
-
-    events: {
-        type: [{ type: Schema.Types.ObjectId, ref:'Event' }],
     }
 })
 
