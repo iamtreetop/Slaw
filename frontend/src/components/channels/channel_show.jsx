2f382a0579abdb958c0267f14a3a5c1120137f19
jonathan-dwight <jd.buendia0711@gmail.com>
2020-10-27 17:39:49 -0700

    Remove Debuggers for Launch

--- frontend/src/components/channels/channel_show.jsx
+++ frontend/src/components/channels/channel_show.jsx
@@ -11,48 +11,8 @@ class ChannelShow extends React.Component {
         this.props.fetchChannel();
     }
 
-    // handleClick(channelId) {
-    //     let newMembers = {
-    //         id: channelId,
-    //         members: this.props.user
-    //     }
-
-    //     this.props.updateChannel(newMembers).then((action) => {
-    //         // this.props.history.push(`/channels/${action.channel.id}`)
-    //     })
-    // }
-
     render() {
-        // if (!this.props.channels) {
-        //     return null;
-        // }
 
-        // const channelForm = (
-        //     <button onClick={() => this.props.openModal('channel')}
-        //         className="channel-form">Create a channel</button>
-        // );
-
-        // // let channelListNotJoined = []
-        // // this.props.channels.map((channel, index)=>{
-        // //     if(channel.members.length > 1) {
-        // //         channel.members.forEach((member) =>{
-        // //             if (member.id === this.props.user.id) {
-        // //                 channelListNotJoined.push(channel)
-        // //             }
-        // //         })
-        // //     } else {
-        // //         channelListNotJoined.push(channel)
-        // //     }
-        // // });
-        // let channelList = this.props.channels.map((channel) => {
-        //     return (
-        //         <li className="channel-list-item">
-        //             <Link className="channel-link" to={"/channels/" + channel._id}>{channel.title}</Link>
-        //             <button className="join-button" onClick={() => this.handleClick(channel._id)}>JOIN</button>
-        //         </li>
-        //     )
-        // });
-        // debugger
         let projectCheck;
         (this.props.channel) ? projectCheck = this.props.channel : projectCheck = null;
         return (
