5fea24a46b23c9849d5a05ee8f9000c7b847d851
jackyli97 <64615179+jackyli97@users.noreply.github.com>
2020-10-28 10:07:55 -0700

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://iamtreetop:9Sq7HAGk4fkTEpTl@cluster0.wpngm.mongodb.net/SlawDB?retryWrites=true&w=majority',
-    secretOrKey: 'SwgV%p/rCzFB^'
-    //Make sure this is your own unique string
-}
