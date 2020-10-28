0847b8858fc607e1f7e09f1f8667b34b3eac4883
Kevin-Lai <kevinlai249@gmail.com>
2020-10-28 11:46:38 -0700

    Fix validator for todos

--- validation/todos.js
+++ validation/todos.js
@@ -4,10 +4,10 @@ const validText = require('./valid-text');
 module.exports = function validateTodoInput(data) {
     let errors = {};
 
-    data.text = validText(data.title) ? data.title : ""
+    data.title = validText(data.title) ? data.title : ""
 
     if (Validator.isEmpty(data.title)) {
-        errors.text = "Title field is required"
+        errors.title = "Title field is required"
     }
 
     return {
