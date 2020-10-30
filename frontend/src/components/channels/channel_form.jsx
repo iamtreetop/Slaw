a6998e5d272031897eca9b0599c297d9ca6d4694
Kevin-Lai <kevinlai249@gmail.com>
2020-10-29 23:12:17 -0700

    Redirect user to general event after channel creation

--- frontend/src/components/channels/channel_form.jsx
+++ frontend/src/components/channels/channel_form.jsx
@@ -54,10 +54,10 @@ class ChannelForm extends React.Component{
 
                 this.props.createChannel(channel).then(
                 (action) => {
-
+                    //debugger
                     this.props.updateUser({channels: action.channel.data._id, id: this.props.user.id});
 
-                    this.props.history.push(`/channels/${action.channel.data._id}`)
+                    this.props.history.push(`/channels/${action.channel.data._id}/${action.channel.data.events[0]}`)
                     this.props.closeModal()
                 }
             ).catch((res) => console.log(res))
