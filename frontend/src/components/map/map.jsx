e3f48b126d4de0dd07f8cb6d1021e41ea16a5a96
jackyli97 <xwlh808@gmail.com>
2020-11-01 20:08:39 -0800

    Add markers to search

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
     GoogleMap,
     useLoadScript,
     Marker,
     InfoWindow,
 } from "@react-google-maps/api";
-
 import UsePlacesAutoComplete, {
     getGeocode,
     getLatLng,
 } from 'use-places-autocomplete';
-import './map.css'
+import './map.css';
+import axios from "axios";
 import {
     Combobox, 
     ComboboxInput,
@@ -26,15 +26,14 @@ require('dotenv').config()
 
 const libraries = ["places"]
 
-
 const mapContainerStyle = {
     height: "100vh",
     width: "100vw",
 };
 
 const center = {
-    lat: 43.6532,
-    lng: -79.3832,
+    lat: 37.774929,
+    lng: -122.419418,
 };
 
 const options = {
@@ -44,36 +43,81 @@ const options = {
 };
 
 export default function SlawMap() {
-    // const onMapClick = React.useCallback((e) => {
-    //     setMarkers((current) => [
-    //         ...current,
-    //         {
-    //             lat: e.latLng.lat(),
-    //             lng: e.latLng.lng(),
-    //             time: new Date(),
-    //         },
-    //     ]);
-    // }, []);
-
-    const [markers, setMarkers] = React.useState([]);
-    const [selected, setSelected] = React.useState(null);
-
     const mapRef = React.useRef();
     const onMapLoad = React.useCallback((map) => {
         mapRef.current = map;
     }, []);
+
+    const [markers, setMarkers] = React.useState([]);
+    const [address, setAddress] = React.useState(null);
+    const [submitted, setSubmitted] = React.useState(false);
 
     const panTo = React.useCallback(({ lat, lng }) => {
         mapRef.current.panTo({ lat, lng });
         mapRef.current.setZoom(14);
     }, []);
 
+    const {
+        ready,
+        value,
+        suggestions: { status, data },
+        setValue,
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: { lat: () => 37.774929, lng: () => -122.419418 },
+            radius: 100 * 1000,
+        },
+    });
+
+    // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+    };
+
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+        setAddress(address)
+        setSubmitted(true)
+        try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+        } catch (error) {
+            console.log("😱 Error: ", error);
+        }
+    };
+
+    useEffect(() => {
+        if (address !== null && markers.length === 0 && submitted ) {
+            // const apiUrl = `https://slaw-app.herokuapp.com/http://api.amp.active.com/v2/search/?near=${encodeURI(address)}&radius=25&query=marathon&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=qcfehs4xxbbcdvepyyn2udsu`;
+            const apiUrl = `https://cors-anywhere.herokuapp.com/http://api.amp.active.com/v2/search/?near=${encodeURI(address)}&radius=25&query=marathon&current_page=1&per_page=10&sort=distance&exclude_children=true&api_key=qcfehs4xxbbcdvepyyn2udsu`;
+            fetch(apiUrl, { method: 'GET', mode: 'cors'})
+                .then(function(res) {
+                    return res.json()
+                })
+                .then(function (data) {
+                    console.log(data)
+                    setMarkers(data.results)
+                })
+                .catch((res) => {
+                    console.log(res);
+                });
+        }
+        setSubmitted(false)
+        }
+    );
+
     const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: "AIzaSyB1_x5gxp3yYVk5wqOuCzxajSRfgZbhkFE",
+        googleMapsApiKey: "AIzaSyDeO5HlPjN1dpNDOgAMVPHZQr3utCmUar0",
         libraries,
     });
+
     if (loadError) return "Error";
     if (!isLoaded) return "Loading...";
+
     return(
         <div className="slaw-map">
             <h1>
@@ -84,18 +128,45 @@ export default function SlawMap() {
             </h1>
 
             <Locate panTo={panTo} />
-            <Search panTo={panTo} />
+            
+            <div className="search">
+                <Combobox onSelect={handleSelect}>
+                    <ComboboxInput
+                        value={value}
+                        onChange={handleInput}
+                        disabled={!ready}
+                        placeholder="Search your location"
+                    />
+                    <ComboboxPopover>
+                        <ComboboxList>
+                            {status === "OK" &&
+                                data.map(({ id, description }) => (
+                                    <ComboboxOption key={id} value={description} />
+                                ))}
+                        </ComboboxList>
+                    </ComboboxPopover>
+                </Combobox>
+            </div>
 
             <GoogleMap mapContainerStyle={mapContainerStyle}
-            zoom={8}
-            center={center}
-            options={options}
-            // onClick={onMapClick}
-            onLoad={onMapLoad}
+                zoom={8}
+                center={center}
+                options={options}
+                // onClick={onMapClick}
+                onLoad={onMapLoad}
             >
-
+            {markers.map((activity, idx) => (
+                <Marker
+                    key={idx}
+                    position={{
+                        lat: Number(activity.place.geoPoint.lat),
+                        lng: Number(activity.place.geoPoint.lon)
+                    }}
+                />
+            ))}
 
             </GoogleMap>
+            
         </div>
     )
     }
@@ -121,57 +192,59 @@ function Locate({ panTo }) {
     );
 }   
 
-function Search({ panTo }) {
-    const {
-        ready,
-        value,
-        suggestions: { status, data },
-        setValue,
-        clearSuggestions,
-    } = usePlacesAutocomplete({
-        requestOptions: {
-            location: { lat: () => 43.6532, lng: () => -79.3832 },
-            radius: 100 * 1000,
-        },
-    });
+// function Active(input)
 
-    // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+// function Search({ panTo }) {
+//     const {
+//         ready,
+//         value,
+//         suggestions: { status, data },
+//         setValue,
+//         clearSuggestions,
+//     } = usePlacesAutocomplete({
+//         requestOptions: {
+//             location: { lat: () => 37.774929, lng: () => -122.419418 },
+//             radius: 100 * 1000,
+//         },
+//     });
 
-    const handleInput = (e) => {
-        setValue(e.target.value);
-    };
+//     // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
 
-    const handleSelect = async (address) => {
-        setValue(address, false);
-        clearSuggestions();
+//     const handleInput = (e) => {
+//         setValue(e.target.value);
+//     };
 
-        try {
-            const results = await getGeocode({ address });
-            const { lat, lng } = await getLatLng(results[0]);
-            panTo({ lat, lng });
-        } catch (error) {
-            console.log("😱 Error: ", error);
-        }
-    };
+//     const handleSelect = async (address) => {
+//         setValue(address, false);
+//         clearSuggestions();
+//         searchResult = address;
+//         try {
+//             const results = await getGeocode({ address });
+//             const { lat, lng } = await getLatLng(results[0]);
+//             panTo({ lat, lng });
+//         } catch (error) {
+//             console.log("😱 Error: ", error);
+//         }
+//     };
 
-    return (
-        <div className="search">
-            <Combobox onSelect={handleSelect}>
-                <ComboboxInput
-                    value={value}
-                    onChange={handleInput}
-                    disabled={!ready}
-                    placeholder="Search your location"
-                />
-                <ComboboxPopover>
-                    <ComboboxList>
-                        {status === "OK" &&
-                            data.map(({ id, description }) => (
-                                <ComboboxOption key={id} value={description} />
-                            ))}
-                    </ComboboxList>
-                </ComboboxPopover>
-            </Combobox>
-        </div>
-    );
-}
+//     return (
+//         <div className="search">
+//             <Combobox onSelect={handleSelect}>
+//                 <ComboboxInput
+//                     value={value}
+//                     onChange={handleInput}
+//                     disabled={!ready}
+//                     placeholder="Search your location"
+//                 />
+//                 <ComboboxPopover>
+//                     <ComboboxList>
+//                         {status === "OK" &&
+//                             data.map(({ id, description }) => (
+//                                 <ComboboxOption key={id} value={description} />
+//                             ))}
+//                     </ComboboxList>
+//                 </ComboboxPopover>
+//             </Combobox>
+//         </div>
+//     );
+// }
